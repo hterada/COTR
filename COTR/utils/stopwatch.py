1fc4147d9ff4a9d9f466043e44dc65eb0578829f
terada.h <terada.h@opst.co.jp>
2023-01-27 07:43:42 +0900

    add: to_DataFrame()

--- COTR/utils/stopwatch.py
+++ COTR/utils/stopwatch.py
@@ -1,6 +1,7 @@
-from typing import List
+from typing import List, Optional
 import time
 from collections import OrderedDict
+import pandas as pd
 
 class StopWatch:
     """Python の with 構文にもとづくストップウォッチ。withブロック内の所要時間を計測する。
@@ -51,19 +52,36 @@ class StopWatch:
         count = __class__.dic_count[str(name_path)]
         level = len(name_path)-1
 
-        if len(name_path) >= 2:
-            parent_sum = self.get_parent_sum(name_path)
-        else:
-            # no parent
-            parent_sum = sum
-
         # root sum
         root_sum = __class__.dic_sum[str(name_path[:1])]*1000
         print(f"SW:[{'.'*level+name_path[-1]:40}] time sum:{sum:8.3f} count:{count:4} avg:{sum/count:8.3f} {sum/root_sum*100.0:6.2f}%")
+        # child
         for child_name in __class__.dic_child_names[str(name_path)].keys():
             child_name_path = list(name_path) #copy
             child_name_path.append(child_name)
             self.print(child_name_path)
+
+    def to_DataFrame(self)->pd.DataFrame:
+        l_out = []
+        data = self.make_table(l_out, self.name_path)
+        columns = ["name", "sum", "count", "avg", "% of root"]
+        df = pd.DataFrame(data, columns=columns)
+        return df
+
+
+    def make_table(self, l_out, name_path):
+        sum = __class__.dic_sum[str(name_path)]*1000
+        count = __class__.dic_count[str(name_path)]
+        level = len(name_path)-1
+        root_sum = __class__.dic_sum[str(name_path[:1])]*1000
+        # add row
+        l_out.append(['.'*level+name_path[-1], sum, count ,sum/count ,sum/root_sum*100.0])
+        # child
+        for child_name in __class__.dic_child_names[str(name_path)].keys():
+            child_name_path = list(name_path) #copy
+            child_name_path.append(child_name)
+            self.make_table(l_out, child_name_path)
+        return l_out
 
     def get_parent_sum(self, child_name_path:List[str])->List[str]:
         if len(child_name_path) < 2:
