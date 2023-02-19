b4e5c86116c2a92fdda812b65b7ae367df47649c
Hideo TERADA <h.tee2006@gmail.com>
2023-02-19 23:19:56 +0900

    edit: /mnt をボリュームに追加

--- docker_run_demo-cuda12.sh
+++ docker_run_demo-cuda12.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-docker run --rm --gpus all -v /home/terada/work/COTR:/COTR \
+docker run --rm --gpus all \
+  -v /home/terada/work/COTR:/COTR \
+  -v /mnt:/mnt \
   -v $HOME/.Xauthority:/root/.Xauthority \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   -e DISPLAY=$DISPLAY \
