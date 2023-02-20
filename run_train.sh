8a745c2457f52f7b6f93630721e22f6237c4e4d8
Hideo TERADA <h.tee2006@gmail.com>
2023-02-21 07:58:16 +0900

    edit: debug+use_ram

--- run_train.sh
+++ run_train.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# org
 #python train_cotr.py --scene_file sample_data/jsons/200_megadepth.json --info_level=rgbd --use_ram=yes --use_cc=no --batch_size=24 --learning_rate=1e-4 --lr_backbone=0 --max_iter=300000 --workers=8 --cycle_consis=yes --bidirectional=yes --position_embedding=lin_sine --layer=layer3 --confirm=no --dataset_name=megadepth_sushi --suffix=stage_1 --valid_iter=1000 --enable_zoom=no --crop_cam=crop_center_and_resize --out_dir=./my_out/cotr --resume True
-python train_cotr.py --scene_file sample_data/jsons/debug_megadepth.json --info_level=rgbd --use_ram=no --use_cc=no --batch_size=24 --learning_rate=1e-4 --lr_backbone=0 --max_iter=300000 --workers=8 --cycle_consis=yes --bidirectional=yes --position_embedding=lin_sine --layer=layer3 --confirm=no --dataset_name=megadepth_sushi --suffix=stage_1 --valid_iter=1000 --enable_zoom=no --crop_cam=crop_center_and_resize --out_dir=./my_out/cotr --resume False
+
+# # debug(proven)
+# python train_cotr.py --scene_file sample_data/jsons/debug_megadepth.json --info_level=rgbd --use_ram=no --use_cc=no --batch_size=24 --learning_rate=1e-4 --lr_backbone=0 --max_iter=300000 --workers=8 --cycle_consis=yes --bidirectional=yes --position_embedding=lin_sine --layer=layer3 --confirm=no --dataset_name=megadepth_sushi --suffix=stage_1 --valid_iter=1000 --enable_zoom=no --crop_cam=crop_center_and_resize --out_dir=./my_out/cotr --resume False
+
+#
+python train_cotr.py --scene_file sample_data/jsons/debug_megadepth.json --info_level=rgbd --use_ram=yes --use_cc=no --batch_size=24 --learning_rate=1e-4 --lr_backbone=0 --max_iter=300000 --workers=8 --cycle_consis=yes --bidirectional=yes --position_embedding=lin_sine --layer=layer3 --confirm=no --dataset_name=megadepth_sushi --suffix=stage_1 --valid_iter=1000 --enable_zoom=no --crop_cam=crop_center_and_resize --out_dir=./my_out/cotr --resume=yes
 
