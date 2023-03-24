c75847b7429dc775899f54f5d3d210a6c0e3d0bb
Hideo TERADA <h.tee2006@gmail.com>
2023-03-24 10:20:15 +0900

    add: distill_6

--- run_distill2.sh
+++ run_distill2.sh
@@ -24,12 +24,24 @@
 #  --confirm=no --dataset_name=megadepth_sushi \
 #  --valid_iter=50 --enable_zoom=no --crop_cam=crop_center_and_resize --out_dir=./my_out --resume=no --cc_resume=yes
 
-# student : layer1, distill_4_full
+# student : layer2, distill_5_full
+# python distill.py --load_t_weights="out/default" \
+#  --load_s_weights="out/default" \
+#  --layer=layer3 --s_layer=layer2 \
+#  --scene_file sample_data/jsons/debug_megadepth.json \
+#  --suffix=distill_5_full \
+#  --info_level=rgbd --use_ram=no --use_cc=no --batch_size=24 --learning_rate=1e-4 --max_iter=100000000 --workers=8 --position_embedding=lin_sine \
+#  --confirm=no --dataset_name=megadepth_sushi \
+#  --valid_iter=50 --enable_zoom=no --crop_cam=crop_center_and_resize --out_dir=./my_out \
+#  --resume=no --cc_resume=yes \
+#  --gpuno=1
+
+# student : layer1, distill_6_full
 python distill.py --load_t_weights="out/default" \
  --load_s_weights="out/default" \
- --layer=layer3 --s_layer=layer2 \
+ --layer=layer3 --s_layer=layer1 \
  --scene_file sample_data/jsons/debug_megadepth.json \
- --suffix=distill_5_full \
+ --suffix=distill_6_full \
  --info_level=rgbd --use_ram=no --use_cc=no --batch_size=24 --learning_rate=1e-4 --max_iter=100000000 --workers=8 --position_embedding=lin_sine \
  --confirm=no --dataset_name=megadepth_sushi \
  --valid_iter=50 --enable_zoom=no --crop_cam=crop_center_and_resize --out_dir=./my_out \
